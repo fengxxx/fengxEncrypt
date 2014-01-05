7e0b928a65f32d9a3a5d8cf3a1c01c6cadf33167
fengx <zengme@gmail.com>
2014-01-05 22:33:26 +0800

    lean struct

--- fengxEncrypt.py
+++ fengxEncrypt.py
@@ -2,22 +2,35 @@
 # -*- coding: cp936 -*-
 import sys      
 import os
+import struct
+fengxMark="fengxEncrypt\n"
+key=[1,2,1,2,1,2,1,2,1,2,1,2,1,2]  
+
+
+
 def encrypt(s,key):
-    #eStr=""
-    eStr=s+"曾哥"
-    #eStr=s[::-1]
+    eStr=s[::-1]
+    for c in s:
+        d=struct.unpack("s",c)
+        print d
+        #for cc in d:
+        #print struct.unpack("i",d)
+    #eStr=s.encode('utf8')
+    #eStr=s.encode('ISO-8859-1')
+    print eStr
+
     return eStr
 
 def decrypt(s,key):
-    #dStr=""
-    #eStr=s[::-1]
-    eStr=s.replace("曾哥", "", [count]) 
+    eStr=s[::-1]
+    #eStr=s.replace(key, [count]) 
     return eStr
 
 def enFile(filePath,targetPath,key):
     fileC=open(filePath,"rb")
     fileT=open(targetPath,"wb")
-    line = fileC.readline()             
+    line = fileC.readline()
+    fileT.write(fengxMark)
     while line:
         fileT.write(encrypt(line,key))
         line = fileC.readline()
@@ -27,23 +40,49 @@ def enFile(filePath,targetPath,key):
 def deFile(filePath,targetPath,key):
     fileC=open(filePath,"rb")
     fileT=open(targetPath,"wb")
-    line = fileC.readline()             
+    line = fileC.readline()
+    line = fileC.readline()
     while line:
         fileT.write(decrypt(line,key))
         line = fileC.readline()
     fileC.close()
     fileT.close()
-
     
-def main():
-    filePath=sys.argv[1]
+def isEncry(filePath):
+    state=False
+    fileC=open(filePath,"rb")
+    line = fileC.readline()  
+    if line==fengxMark:
+       state=True
+    fileC.close()
+    return state
+    
+    
+def fengxEorD(filePath,key):
     tS=os.path.split(filePath)
-    fileOutPath=tS[0]+"\\"+os.path.splitext(tS[1])[0] +"_fengx" + os.path.splitext(tS[1])[1]
-    enFile(filePath,fileOutPath,"s")
-    print ("加密文件-->>"+filePath)
-    print ("输出文件-->>"+fileOutPath)
-main()
+    print isEncry(filePath)
+    if isEncry(filePath)==True: 
+        print "true"
+        fileOutPath=tS[0]+"\\"+os.path.splitext(tS[1])[0] +"_decrypt" + os.path.splitext(tS[1])[1]
+        deFile(filePath,fileOutPath,key)
+        print ("解密文件-->>"+filePath)
+        print ("输出文件-->>"+fileOutPath)
+    else:
+        fileOutPath=tS[0]+"\\"+os.path.splitext(tS[1])[0] +"_encrypt" + os.path.splitext(tS[1])[1]
+        enFile(filePath,fileOutPath,key)
+        print ("加密文件-->>"+filePath)
+        print ("输出文件-->>"+fileOutPath)
 
+
+try:
+    sys.argv[1]
+except:
+    print "没有拖拽！"
+else:
+    fengxEorD(sys.argv[1],"s")
+
+
+test="E:\\GitHub\\fengxEncrypt\\bghm.prefab"
+fengxEorD(test,"s")
+ 
 raw_input()
-
-#str.startswith('str')
---
+++ test struct.py
@@ -0,0 +1,11 @@
+# -*- coding: cp936 -*-
+import struct  
+ 
+a = 20 
+b = 400 
+c="¶¼ÊÇ"
+#str = struct.pack("ii", a, b)  
+#print 'length:', len(str)  
+#print str  
+#print repr(str)  
+ 
